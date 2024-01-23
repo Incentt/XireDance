a96ff35a646b01128b66f0604b41f0261916dc94
Vincent Wisnata <wisnatavincent@gmail.com>
2024-01-23 17:42:22 +0700

    Fima

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,25 +5,49 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public float timer = 120f;
     public GameObject playText;
     public AudioSource theMusic;
     public bool startGame;
     public int score;
+    public Text scoreUI;
     public int HitNote;
     public int MissNote;
     public GameObject miss, bad, cool, great, perfect;
-    
+    private bool isOnce;
+    public Animator anim;
+    public Text perfectText;
+    public GameObject restartButt;
+
+    public Text missCount;
+    private int hitCount;
     private void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && isOnce == false)
         {
             startGame = true;
             playText.SetActive(false);
+            theMusic.Play();
+            isOnce = true;
+            anim.SetBool("isPlay", true);
         }
+        scoreUI.text = score.ToString();
+        perfectText.text = "Hit: " + hitCount.ToString();
+        missCount.text = "Miss: " + MissNote.ToString();
+        if(isOnce == true)
+        {
+            timer -= Time.deltaTime;
+            
+        }
+        if(timer <= 0)
+        {
+            restartButt.SetActive(true);
+        }
+        
     }
     public void noteHit()
     {
-        HitNote += 1;
+        hitCount += 1;
     }
     public void noteMiss()
     {
--- Assets/Script/beatScroll.cs
+++ Assets/Script/beatScroll.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class beatScroll : MonoBehaviour
 {
     public float speed = 2f;
-
+    public bool canPress;
     public KeyCode keyPress;
     private GameManager gm;
     public bool miss = false;
@@ -20,65 +20,82 @@ public class beatScroll : MonoBehaviour
         {
             transform.position -= new Vector3(0, speed * Time.deltaTime, 0f);
         }
-        if (transform.localPosition.y < -1)
-        {
-            miss = true;
-        }
-        if (miss)
+     
+ 
+        if(transform.localPosition.y < -1)
         {
             gm.noteMiss();
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
-            if (Input.GetKeyDown(keyPress) && Mathf.Abs(transform.localPosition.y) > 1.3f)
+        if (Input.GetKeyDown(keyPress))
+        {
+            if (canPress)
             {
                 
-                if (Mathf.Abs(transform.localPosition.y) > 1.3f)
+                if (Input.GetKeyDown(keyPress))
                 {
-                Debug.Log("miss");
-                gm.miss.SetActive(true);
-                gm.score += 10;
+                    
+                    gameObject.SetActive(false);
+                    if (Mathf.Abs(transform.localPosition.y) > 0.8f)
+                    {
+                        Debug.Log("miss");
+                        gm.miss.SetActive(true);
+                        gm.score += 10;
+                        gm.noteMiss();
+                    }
+                    else if (Mathf.Abs(transform.localPosition.y) > 0.7f)
+                    {
+                        Debug.Log("bad");
+                        gm.bad.SetActive(true);
+                        gm.score += 20;
+                        gm.noteHit();
+                    }
+                    else if (Mathf.Abs(transform.localPosition.y) > 0.5f)
+                    {
+                        Debug.Log("cool");
+                        gm.cool.SetActive(true);
+                        gm.score += 30;
+                        gm.noteHit();
+                    }
+                    else if (Mathf.Abs(transform.localPosition.y) > 0.25f)
+                    {
+                        Debug.Log("great");
+                        gm.great.SetActive(true);
+                        gm.score += 40;
+                        gm.noteHit();
+                    }
+                    else if (Mathf.Abs(transform.localPosition.y) > 0.05f)
+                    {
+                        Debug.Log("perfect");
+                        gm.perfect.SetActive(true);
+                        gm.score += 50;
+                        gm.noteHit();
+                    }
+                    else
+                    {
+                        Debug.Log("perfect");
+                        gm.perfect.SetActive(true);
+                        gm.score += 50;
+                        gm.noteHit();
+                    }
+                    
                 }
-                else if (Mathf.Abs(transform.localPosition.y) > 1f)
-                {
-                Debug.Log("bad");
-                gm.bad.SetActive(true);
-                gm.score += 20;
-            }
-                else if (Mathf.Abs(transform.localPosition.y) > 0.5f)
-                {
-                Debug.Log("cool");
-                gm.cool.SetActive(true);
-                gm.score += 30;
-            }
-                else if (Mathf.Abs(transform.localPosition.y) > 0.25f)
-                {
-                Debug.Log("great");
-                gm.great.SetActive(true);
-                gm.score += 40;
-            }
-                else if (Mathf.Abs(transform.localPosition.y) > 0.05f)
-                {
-                Debug.Log("perfect");
-                gm.perfect.SetActive(true);
-                gm.score += 50;
-            }
-                else if (Mathf.Abs(transform.localPosition.y) == 0f)
-                {
-                Debug.Log("perfect");
-                gm.perfect.SetActive(true);
-                gm.score += 50;
-            }
-                Destroy(gameObject);
-            gm.noteHit();
 
             }
-       
+        }
+        
         
 
         
 
     }
 
-   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("buttPress"))
+        {
+            canPress = true;
+        }
+    }
 
 }
